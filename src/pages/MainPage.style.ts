caefb2b59ae760868b9f0843d7139d8f3393c957
taehunkim <117618060+thisistaehun@users.noreply.github.com>
2024-11-30 17:58:56 +0900

    add header

---
+++ src/pages/MainPage.style.ts
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #2d2d2d;
+  color: #ffffff;
+  height: 100vh;
+`;
+
+export const BorderLine = styled.div`
+  width: 97%;
+  border-bottom: 1px solid #ffffff;
+  margin: 0 auto;
+  text-align: center;
+  font-size: 28px;
+  font-weight: 400;
+  padding: 10px;
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const HeaderLeftWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-direction: column;
+`;
+
+export const HeaderRightWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+export const Name = styled.h1`
+  font-size: 28px;
+  color: #ffffff;
+  margin: 0px 8px;
+`;
+
+export const Job = styled.h2`
+  font-size: 18px;
+  font-weight: 400;
+  margin: 0px 10px;
+  color: #cccccc;
+`;
+
+export const GithubImage = styled.img`
+  width: 28px;
+  height: 28px;
+  margin: 0px 8px;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const EmailAddress = styled.text`
+  margin: 10px;
+  font-size: 18px;
+  font-weight: 400;
+  color: #cccccc;
+  margin: 0px 12px;
+`;
+
+export const Body = styled.div``;
+export const Footer = styled.footer``;
---
+++ src/types/typings.d.ts
@@ -0,0 +1,39 @@
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: number;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: number;
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: number;
+  export default content;
+}
+
+declare module "*.mp3" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.wav" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.mp4" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.mov" {
+  const content: string;
+  export default content;
+}
